496dc33733bf7518b52f1f3216adca197d157826
Isaac Mok Kai Xiang <isaac.mok.kx@gmail.com>
2021-01-25 11:12:25 +0800

    fix

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -12,6 +12,9 @@
             <label for="password" class="w-28">Password</label>
             <input type="password" name="password" placeholder="Password" class="mx-4 flex-1 shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline block my-2 w-full">
         </div>
+        @if (request()->has('requested'))
+        <input type="hidden" name="requested" value="{{ request()->requested }}">
+        @endif
         <button type="submit" class="my-2 px-4 py-2 bg-gray-300 rounded">Log in</button>
     </form>
     </x-bigmom-auth::card>
